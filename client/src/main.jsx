beafb01975a884227800a6de07f5803e292e3620
Jivansatapathy <satapathyjjivan@gmail.com>
2025-09-12 21:08:55 +0530

    first

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 // client/src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
@@ -8,7 +9,9 @@ import { AuthProvider } from "./context/AuthContext";
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </AuthProvider>
   </React.StrictMode>
 );

8f9d8792bc5fda424dc3fe6b8f2e36c225c41168
Jivansatapathy <satapathyjjivan@gmail.com>
2025-11-22 21:38:17 +0530

    Update gallery event names: remove 'Meeting' and capitalize 'With'

--- client/src/data/galleryData.js
+++ client/src/data/galleryData.js
@@ -13,7 +13,7 @@ export const galleryData = [
   {
     id: 1,
     image: "/Gallery/22nd Prime Minister of Canada, Stephen Harper.jpg",
-    eventName: "Meeting with Stephen Harper - 22nd Prime Minister of Canada",
+    eventName: "With Stephen Harper - 22nd Prime Minister of Canada",
     location: "Canada",
     description: "Please add event description here.",
     attendees: "Please add who met with whom",
@@ -22,7 +22,7 @@ export const galleryData = [
   {
     id: 2,
     image: "/Gallery/Associate Minister of Small Business Nina Tangri.jpg",
-    eventName: "Meeting with Nina Tangri - Associate Minister of Small Business",
+    eventName: "With Nina Tangri - Associate Minister of Small Business",
     location: "Canada",
     description: "Please add event description here.",
     attendees: "Please add who met with whom",
@@ -31,7 +31,7 @@ export const galleryData = [
   {
     id: 3,
     image: "/Gallery/Baxter Hunt, Consulate General of the U.S. Embassy in Toronto.jpg",
-    eventName: "Meeting with Baxter Hunt - Consulate General of the U.S. Embassy in Toronto",
+    eventName: "With Baxter Hunt - Consulate General of the U.S. Embassy in Toronto",
     location: "Toronto, Canada",
     description: "Please add event description here.",
     attendees: "Please add who met with whom",
@@ -49,7 +49,7 @@ export const galleryData = [
   {
     id: 5,
     image: "/Gallery/Pavi Binning, President of Whittington Investments Ltd.jpg",
-    eventName: "Meeting with Pavi Binning - President of Whittington Investments Ltd",
+    eventName: "With Pavi Binning - President of Whittington Investments Ltd",
     location: "Please update location",
     description: "Please add event description here.",
     attendees: "Please add who met with whom",
@@ -58,7 +58,7 @@ export const galleryData = [
   {
     id: 6,
     image: "/Gallery/Principal Commerce Officer Bardley Hacker.jpg",
-    eventName: "Meeting with Bardley Hacker - Principal Commerce Officer",
+    eventName: "With Bardley Hacker - Principal Commerce Officer",
     location: "Please update location",
     description: "Please add event description here.",
     attendees: "Please add who met with whom",
@@ -67,7 +67,7 @@ export const galleryData = [
   {
     id: 7,
     image: "/Gallery/the High Commissioner of India..jpg",
-    eventName: "Meeting with the High Commissioner of India",
+    eventName: "With the High Commissioner of India",
     location: "Please update location",
     description: "Please add event description here.",
     attendees: "Please add who met with whom",
@@ -76,7 +76,7 @@ export const galleryData = [
   {
     id: 8,
     image: "/Gallery/Tiff Macklem, the Governor of the Bank of Canada.jpg",
-    eventName: "Meeting with Tiff Macklem - Governor of the Bank of Canada",
+    eventName: "With Tiff Macklem - Governor of the Bank of Canada",
     location: "Canada",
     description: "Please add event description here.",
     attendees: "Please add who met with whom",

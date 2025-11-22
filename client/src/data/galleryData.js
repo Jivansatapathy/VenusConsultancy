44592b3c933775bdd5976a6620d63cdef16f39f9
Jivansatapathy <satapathyjjivan@gmail.com>
2025-11-22 21:48:05 +0530

    Remove Pavi Binning image from gallery

--- client/src/data/galleryData.js
+++ client/src/data/galleryData.js
@@ -47,15 +47,6 @@ export const galleryData = [
     orientation: "landscape"
   },
   {
-    id: 5,
-    image: "/Gallery/Pavi Binning, President of Whittington Investments Ltd.jpg",
-    eventName: "With Pavi Binning - President of Whittington Investments Ltd",
-    location: "Please update location",
-    description: "Please add event description here.",
-    attendees: "Please add who met with whom",
-    orientation: "landscape"
-  },
-  {
     id: 7,
     image: "/Gallery/the High Commissioner of India..jpg",
     eventName: "With the High Commissioner of India",
